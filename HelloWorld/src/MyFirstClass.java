3089f2620d44dfe9a39f045d88a1552ef7deb52e
AdrianPena <AdrianPena@9.84.100.230>
2017-06-02 12:22:23 +0300

    new edit

--- HelloWorld/src/MyFirstClass.java
+++ HelloWorld/src/MyFirstClass.java
@@ -3,7 +3,8 @@ public class MyFirstClass {
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello dark world!");
+		System.out.println("Hello dark  world!");
 		System.out.println("Edit from GIT");
+		System.out.println("new edit");
 	}
 }
